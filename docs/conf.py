e2e7644a52fa6d17716a9b89d6e8d78a7d65ec55
Askepios-ai <ono016@uit.no>
2023-09-12 03:11:06 +0200

    chore: update README

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
@@ -11,7 +15,6 @@ copyright = "2023, Øyvind Nohr"
 author = "Øyvind Nohr"
 
 # -- General configuration ---------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
