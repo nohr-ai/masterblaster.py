363d7d3124c248d7a4dd76493a6c10eb22d7156b
Moritz Jörg <moke@mailbox.org>
2023-09-13 19:51:34 +0200

    chore: cleanup old release

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "masterblaster.py"
 copyright = "2023, Øyvind Nohr"
 author = "Øyvind Nohr"
-version = "0.1.0-alpha.1"
+version = "0.0.6"
 
 # -- General configuration ---------------------------------------------------
 # Add any Sphinx extension module names here, as strings. They can be
